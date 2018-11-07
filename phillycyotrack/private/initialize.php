c13d62dd103c61b745e635a9f4183e5329d87035
h-timmy <33536933+h-timmy@users.noreply.github.com>
2018-11-06 19:11:28 -0500

    Update initialize.php

--- phillycyotrack/private/initialize.php
+++ phillycyotrack/private/initialize.php
@@ -13,11 +13,12 @@
   // * Use same document root as webserver
   // * Can set a hardcoded value:
   define("MY_ROOT", 'C:\Users\runni\Sites\phillycyotrack');
+  define(""WWW_ROOT", 'phillycyotrack/public');
   //define("WWW_ROOT", '');
   // * Can dynamically find everything in URL up to "/public"
-  $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
-  $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
-  define("WWW_ROOT", $doc_root);
+  //$public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
+  //$doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+  //define("WWW_ROOT", $doc_root);
 
   require_once('functions.php');
   require_once('database.php');
