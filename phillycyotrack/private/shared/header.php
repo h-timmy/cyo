7e1d37abbba618714f6abae160ba2bd3bbebde1a
h-timmy <33536933+h-timmy@users.noreply.github.com>
2018-11-24 11:05:00 -0500

    Update header.php

--- phillycyotrack/private/shared/header.php
+++ phillycyotrack/private/shared/header.php
@@ -21,7 +21,7 @@
     	<h1>Philly CYO Track and Field</h1>
       <div id = "navbar">
         <h2><a href="<?php echo url_for('/');?>">Home</a></h2>
-        <h2><a href="<?php echo 'Meets/index.php'; ?>">Meets</a></h2>
+        <h2><a href="<?php echo 'Athletes/index.php'; ?>">Meets</a></h2>
         <h2><a href="<?php echo url_for('/Athletes/index.php'); ?>">Athletes</a></h2>
         <h2><a href="<?php echo url_for('/Teams/index.php'); ?>">Teams</a></h2>
     </div>
